34611e2d1e1874689c681e64431c28c3e2d9cea2
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-09 17:52:16 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -34,25 +34,7 @@ def note_contents
   self.artist_name = Artist.find_or_create_by(name: name)
 end
 
-def category_name
-   self.artist ? self.artist.name : nil
-end
 
-def genre_name=(name)
-self.genre_name = Genre.find_or_create_by(name: name)
-end
-
-def genre_name
- self.genre ? self.genre.name : nil
-end
-
-def note_contents=(content)
-self.note_contents = Note.find_or_create_by(content: content)
-end
-
-def note_contents
- self.note ? self.note.content : nil
-end
 
 
 end
